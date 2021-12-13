cd75b6bfcba412477019fa7d6052d1d8e8e30b48
PratikTalawadekar <pratikt1212@gmail.com>
2021-12-13 21:04:05 +0530

    Added multiply.java

---
+++ multiply.java
@@ -0,0 +1,14 @@
+class multiply
+{
+    public static void main(String[] Prt)
+    {
+        int a = 10;
+        int b = 20;
+        int sum = a*b;
+
+        System.out.println(" a :" +a);
+        System.out.println(" b :" +b);
+        System.out.println("Sum is :" +sum);
+        
+    }
+}
