5330193720bdd57467209531fc14dfceffa68c0e
PratikTalawadekar <pratikt1212@gmail.com>
2021-12-13 17:57:45 +0530

    commited

---
+++ diff.java
@@ -0,0 +1,14 @@
+public class diff
+{
+    public static void main(String[] Prt)
+    {
+        int a = 10;
+        int b = 20;
+        int diff = a-b;
+
+        System.out.println(" a :" +a);
+        System.out.println(" b :" +b);
+        System.out.println("diff is :" +diff);
+        
+    }
+} 
